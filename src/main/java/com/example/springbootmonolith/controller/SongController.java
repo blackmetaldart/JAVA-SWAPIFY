3677ba6a98f6f5681ff6ec8e1b356e3e11f5b886
Alexander <mrbeachcode@gmail.com>
2019-10-02 11:49:28 -0400

    Testing the functionality of the site. Removed test files.

--- src/main/java/com/example/springbootmonolith/controller/SongController.java
+++ src/main/java/com/example/springbootmonolith/controller/SongController.java
@@ -4,6 +4,7 @@ package com.example.springbootmonolith.controller;
 import com.example.springbootmonolith.models.Song;
 import com.example.springbootmonolith.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +14,7 @@ public class SongController {
     @Autowired
     SongService songService;
 
+    @PreAuthorize("hasRole('ROLE_ARTIST')")
     @PostMapping
     public Song createSong(@RequestBody Song song){
         return songService.createSong(song);
--- src/main/java/com/example/springbootmonolith/models/UserProfile.java
+++ src/main/java/com/example/springbootmonolith/models/UserProfile.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user_profile")
+@Table(name = "userProfile")
 public class UserProfile {
 
     @Id
