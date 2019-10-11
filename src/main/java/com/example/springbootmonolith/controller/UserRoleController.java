33087b9eaf221b4d512d6d5d4bd5b6b8e682ee27
Alexander <mrbeachcode@gmail.com>
2019-10-11 19:04:18 -0400

    commented the user role controller for readability

--- src/main/java/com/example/springbootmonolith/controller/UserRoleController.java
+++ src/main/java/com/example/springbootmonolith/controller/UserRoleController.java
@@ -3,6 +3,7 @@ package com.example.springbootmonolith.controller;
 import com.example.springbootmonolith.models.UserRole;
 import com.example.springbootmonolith.service.UserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,11 +16,16 @@ public class UserRoleController {
     public void setUserRoleService(UserRoleService userRoleService){
         this.roleService=userRoleService;
     }
+
+    //THIS METHOD ALLOWS A USER WITH THE ROLE OF ADMIN TO SEE A LIST OF THOSE WITH SPECIFIC ACCESS
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{rolename}")
     public UserRole getRole(@PathVariable String rolename) {
         return roleService.getRole(rolename);
     }
 
+    //THIS METHOD ALLOWS A USER WITH THE ROLE OF ADMIN TO CREATE A ROLE TO GRANT ACCESS
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/createRole")
     public UserRole createRole(@RequestBody UserRole role) {
         return roleService.createRole(role);
