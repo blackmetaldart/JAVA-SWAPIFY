79b0ca5bcbbe018d1e58277809c1a340f099ed28
Alexander <mrbeachcode@gmail.com>
2019-11-29 21:18:34 -0500

    commented the user role repository

--- src/main/java/com/example/springbootmonolith/repositories/UserRoleRepository.java
+++ src/main/java/com/example/springbootmonolith/repositories/UserRoleRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRoleRepository extends CrudRepository<UserRole, Integer> {
 
+    // FIND A USER ROLE WITH A NAME
     public UserRole findByName(String name);
 }
