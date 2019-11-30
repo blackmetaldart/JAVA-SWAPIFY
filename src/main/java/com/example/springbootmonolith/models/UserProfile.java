bf7aca6c4379e50777475ade66793a0e0ba76932
Alexander <mrbeachcode@gmail.com>
2019-11-29 21:23:34 -0500

    commented the user profile model

--- src/main/java/com/example/springbootmonolith/models/UserProfile.java
+++ src/main/java/com/example/springbootmonolith/models/UserProfile.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Table(name = "userProfile")
 public class UserProfile {
 
-    //ID / EMAIL/ MOBILE/ ADDRESS / USERID COLUMNS FOR TABLES
+    // ID / EMAIL/ MOBILE/ ADDRESS / USERID COLUMNS FOR TABLES
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,53 +26,33 @@ public class UserProfile {
     @Column
     private Long userId;
 
-    //MAPS THE USER PROFILES TO THE USERS
+    // MAPS THE USER PROFILES TO THE USERS
     @JsonIgnore
     @OneToOne(mappedBy = "userProfile",
             cascade = {CascadeType.DETACH, CascadeType.MERGE,
                     CascadeType.PERSIST, CascadeType.REFRESH})
     private User user;
 
-    //EMPTY USERPROFILE CONSTRUCTOR
+    // EMPTY USERPROFILE CONSTRUCTOR
     public UserProfile(){}
 
-    //GETTERS AND SETTERS FOR THE COLUMNS
+    // GETTERS AND SETTERS FOR THE COLUMNS
     public User getUser() { return user; }
-
     public void setUser(User user) { this.user = user; }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public String getMobile() { return mobile; }
+    public void setMobile(String mobile) { this.mobile = mobile; }
 
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 
     public Long getUserId() {return userId; };
-
     public void setUserId(Long userId) {this.userId = userId;};
 
 }
