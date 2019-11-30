ef1b6956e285a4beec75dcbc49ddce9ec0558a5a
Alexander <mrbeachcode@gmail.com>
2019-11-29 21:19:31 -0500

    commented the user repository

--- src/main/java/com/example/springbootmonolith/repositories/UserRepository.java
+++ src/main/java/com/example/springbootmonolith/repositories/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    // RETURNS A USER WITH A USERNAME AND PASSWORD
     @Query("FROM User u WHERE u.username = ?1 and u.password = ?2")
     public User login(String username, String password);
 
+    // RETURNS A USER WITH A USERNAME
     public User findByUsername(String username);
 
 }
