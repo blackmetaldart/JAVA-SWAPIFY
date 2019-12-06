2a9bf5c6b3c8e7c8303d86c450ddf916bdb10347
Alexander <mrbeachcode@gmail.com>
2019-12-06 18:43:49 -0500

    changed the id type for userRole from int to Long

--- src/main/java/com/example/springbootmonolith/models/UserRole.java
+++ src/main/java/com/example/springbootmonolith/models/UserRole.java
@@ -18,7 +18,7 @@ public class UserRole {
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(unique = true)
     private String name;
@@ -42,8 +42,8 @@ public class UserRole {
     public void setUsers(List<User> users){ this.users = users; }
     public List<User> getUsers(){ return users; }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
